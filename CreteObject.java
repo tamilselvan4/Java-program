ae1a16e973a9f1b273561102b67baf1558c4ffe5
Tamilselvan <st711120104079@jit.ac.in>
2023-10-10 18:58:31 +0530

    instance task

---
+++ CreteObject.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class CreteObject {
+
+
+    private static Set<Class> instances = new HashSet<>();
+    public CreteObject() {
+        if (instances.contains(this.getClass())) {
+            throw new IllegalStateException("Object is already created");
+        }
+        instances.add(getClass());
+    }
+}
+
+class B extends CreteObject {
+    
+}
+
+class C extends CreteObject {
+    
+}
+
+class CreateObject {
+    public static void main(String[] args) {
+        B b = new B();
+        B b1 = new B();
+    }
+}
